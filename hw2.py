a2ee9231f1e4b212d3a74b185f61091e3350e391
Andrii Banduliak <banduliak1303@gmail.com>
2022-09-27 18:11:21 +0300

    3rd task is done

--- hw2.py
+++ hw2.py
@@ -1,24 +1,13 @@
-'''4. Напишите программу, которая принимает 
-на вход 2 числа. Задайте список из N элементов, 
-заполненных числами из промежутка [-N, N]. 
-Найдите произведение элементов на указанных 
-позициях(не индексах).
-Position one: 1
-Position two: 3
-Number of elements: 5
--> [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5]
--> 15'''
-num1 = int(input("Enter 1st number "))
-num2 = int(input("Enter 2nd number "))
-num1 *= -1
+"""3. Задайте список из n чисел, заполненный 
+по формуле (1 + 1/n) ** n и выведите на экран их сумму.
+Для n = 6: [2, 2, 2, 2, 2, 3] -> 13"""
 
-a = list(range(num1,num2+1))
-pos1 = int(input('Enter 1st position '))
-pos2 = int(input("Enter 2nd position "))
 
-pos1 -=1
-pos2 -=1
-e = a[pos1] * a[pos2]
+a_int = int(input("> "))
 
-print(a)
-print(e)
+int_list = []
+
+for i in range(1, a_int + 1):
+    int_list.append( int(round( ( (1 + 1/i) ** i), 0)))
+ 
+print(f"Для n = {a_int}: {int_list} -> {sum(int_list)}")
